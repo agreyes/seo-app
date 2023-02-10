8897899c5a8eade1700d72c634d728fdf0d39405
agreyes <alexmvj@gmail.com>
2023-02-10 13:50:19 -0500

    Adding landing page

--- components/home/component-grid.tsx
+++ components/home/component-grid.tsx
@@ -3,53 +3,38 @@ import { useDemoModal } from "@/components/home/demo-modal";
 import Popover from "@/components/shared/popover";
 import Tooltip from "@/components/shared/tooltip";
 import { ChevronDown } from "lucide-react";
+import https from 'https';
 
 export default function ComponentGrid() {
-  const { DemoModal, setShowDemoModal } = useDemoModal();
-  const [openPopover, setOpenPopover] = useState(false);
+  const [state, setState] = useState({
+    searchText: 'efiling integration',
+    targetUrl: 'www.infoTrack.com',
+  });
+
+  const handleChange = (key : string) => (e : React.FormEvent<HTMLInputElement>) => {
+    setState({ ...state, [key]: e.currentTarget.value });
+  }
+
+  const onClick = () => {
+    console.log(state);
+  }
+
   return (
     <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
-      <DemoModal />
-      <button
-        onClick={() => setShowDemoModal(true)}
-        className="flex w-40 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
-      >
-        <p className="text-gray-600">Modal</p>
-      </button>
-      <Popover
-        content={
-          <div className="w-full rounded-md bg-white p-2 sm:w-40">
-            <button className="flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100 active:bg-gray-200">
-              Item 1
-            </button>
-            <button className="flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100 active:bg-gray-200">
-              Item 2
-            </button>
-            <button className="flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100 active:bg-gray-200">
-              Item 3
-            </button>
-          </div>
-        }
-        openPopover={openPopover}
-        setOpenPopover={setOpenPopover}
-      >
-        <button
-          onClick={() => setOpenPopover(!openPopover)}
-          className="flex w-40 items-center justify-between rounded-md border border-gray-300 px-4 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
-        >
-          <p className="text-gray-600">Popover</p>
-          <ChevronDown
-            className={`h-4 w-4 text-gray-600 transition-all ${
-              openPopover ? "rotate-180" : ""
-            }`}
-          />
-        </button>
-      </Popover>
-      <Tooltip content="Precedent is an opinionated collection of components, hooks, and utilities for your Next.js project.">
-        <div className="flex w-40 cursor-default items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100">
-          <p className="text-gray-600">Tooltip</p>
-        </div>
+      <Tooltip content="Search text">
+        <input type="text" value={state.searchText} onChange={handleChange('searchText')} placeholder="efiling integration" className="flex w-50 cursor-default items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100">
+        </input>
       </Tooltip>
+      <Tooltip content="Target url">
+        <input type="text" value={state.targetUrl} onChange={handleChange('targetUrl')}placeholder="www.infoTrack.com" className="flex w-50 cursor-default items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100">
+        </input>
+      </Tooltip>
+      <button
+        onClick={onClick}
+        className="flex w-20 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
+      >
+        <p className="text-gray-600">Search</p>
+      </button>
     </div>
   );
 }
